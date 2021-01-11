b7a9afeea546c826f4917eb4c2eb9d7a4112743e
willfeliz1 <william_mfelizardo@hotmail.com>
2021-01-11 12:09:20 -0300

    feat: Criar usuarios | Tipagem para inserção de dados

--- graphql/src/graphql/users/UserResolver.ts
+++ graphql/src/graphql/users/UserResolver.ts
@@ -1,15 +1,37 @@
-import { Query, Resolver } from "type-graphql";
+import { Arg, Field, InputType, Mutation, Query, Resolver } from "type-graphql";
 import UserSchema from "../../model/UserSchema";
 import User from "./User";
 
+@InputType()
+class UserInput {
+  @Field()
+  name: String;
+  
+  @Field()
+  Address: String;
+  
+  @Field()
+  city: String;
+  
+  @Field()
+  phone: String;
+}
+
 @Resolver(User)
 class UserResolver {
-  
+
   @Query(() => [User])
   async users() {
     const users = await UserSchema.find();
 
     return users;
+  }
+
+  @Mutation(() => User)
+  async createUser(@Arg("userInput") userInput: UserInput) {
+    const user = await UserSchema.create(userInput);
+
+    return user;
   }
 }
 
